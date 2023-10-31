89618b36d657af26808ce90688a3acacaa558589
nefelibatala1993 <leuela1993@gmail.com>
2023-10-31 08:44:12 +0300

    second and third task complete

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Basic Flask App With Flask Babel"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config for all Babel Instance"""
+    LANGUAGES = ['en', 'fr']
+
+    # Set the default_locale and the timezone
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Gets the locale from the request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Home Page for the Application"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
