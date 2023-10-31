58736e4f6bc0b2b092ab1ffa6c39debcfdbe04b3
nefelibatala1993 <leuela1993@gmail.com>
2023-10-31 08:01:06 +0300

    finish the first task

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,9 +2,13 @@
 """Basic Flask App"""
 from flask import Flask, render_template
 
-app = Flask()
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def home() -> None:
     return render_template('0-index.html')
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
