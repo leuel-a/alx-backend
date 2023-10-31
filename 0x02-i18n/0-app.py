ef74e3fc3749aef6a95bf81ec8b0b65f7b684a03
nefelibatala1993 <leuela1993@gmail.com>
2023-10-31 08:30:44 +0300

    add the get_locale function to the root

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)  # i18n and l10 support
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+
+    # Set the default time zone and locale
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
