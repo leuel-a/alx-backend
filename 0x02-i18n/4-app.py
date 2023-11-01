5fdd2851a580f99c23b728098e1d699592f87657
nefelibatala1993 <leuela1993@gmail.com>
2023-11-01 04:37:45 +0300

    finish the fifth taks

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Basic Flask App With Flask Babel"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config:
+    """Config for all Babel Instance"""
+    LANGUAGES = ['en', 'fr']
+
+    # Set the default_locale and the timezone
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> None:
+    """Gets the locale from the request"""
+    locale = request.args.get('locale')
+    if locale and locale in ['en', 'fr']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Home Page for the Application"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
