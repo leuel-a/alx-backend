be8c960c14363c2af9988cafcf188c7d980806cd
nefelibatala1993 <leuela1993@gmail.com>
2023-11-28 11:14:08 +0300

    add the content of the second taks

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,23 @@
+import redis, { createClient } from 'redis';
+
+const client = createClient()
+  .on('error', (err) =>
+    console.log(`Redis client not connected to the server: ${err}`)
+  )
+  .on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    redis.print(`Reply: ${reply}`);
+  });
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
