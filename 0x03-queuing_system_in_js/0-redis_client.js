bc9cacd44eccb9a20a1432b4784c5517b2f0b9d8
nefelibatala1993 <leuela1993@gmail.com>
2023-11-28 11:13:54 +0300

    update the content of the first task

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -5,3 +5,5 @@ const client = createClient()
     console.log(`Redis client not connected to the server: ${err}`)
   )
   .on('connect', () => console.log('Redis client connected to the server'));
+
+  
