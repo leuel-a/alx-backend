df2f87c627f07478e3cf9e4dd725fcd8f7cb2e4f
leuel-a <leuela1993@gmail.com>
2024-08-31 20:14:36 +0300

    clean slate

--- 0x03-queuing_system_in_js/0-redis_client.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'redis';
-
-const client = createClient()
-  .on('connect', () => {
-    console.log('Redis connected to the server');
-  })
-  .on('error', (error) => {
-    console.log('Redis not connected to the server: ', error.message);
-  });
